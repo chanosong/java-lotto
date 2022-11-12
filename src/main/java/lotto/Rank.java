f8a60b2cecdbf0bb9c9ba456dc8db114cba67582
chener <49267211+chanosong@users.noreply.github.com>
2022-11-12 21:57:01 +0900

    feat(Rank): Generate Rank Enum
    + Rank: Enum class for rank system

---
+++ src/main/java/lotto/Rank.java
@@ -0,0 +1,18 @@
+package lotto;
+
+public enum Rank {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1400000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0,0);
+
+    private final int match;
+    private final int prize;
+
+    Rank(int match, int prize) {
+        this.match = match;
+        this.prize = prize;
+    }
+}
